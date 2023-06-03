19566f110b81ce6625c27a54acf58af2f506dd15
Guilherme <guicardosodeoliveira@gmail.com>
2023-06-03 17:42:53 -0300

    Header pronto e início da tela login

--- 4food-project/src/app/layout.tsx
+++ 4food-project/src/app/layout.tsx
@@ -1,16 +1,19 @@
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
-
 import { Inter } from "next/font/google";
 import "./globals.css";
+import CSS from 'csstype';
 import React, { ReactElement } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const bodyStyle: CSS.Properties = {
+  paddingTop: '80px',
+};
 export default function RootLayout({ children }: { children: ReactElement }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className} style={bodyStyle}>
         <Header />
         {children}
         <Footer />
---
+++ 4food-project/src/app/login/page.tsx
@@ -0,0 +1,8 @@
+export default function Login() {
+    return (
+      <div>
+        <h1>Opa aqui é o Login</h1>
+      </div>
+    );
+  }
+  
