a70313ce98499d1b48bf5bea3a7e57e46f21dc52
lorenzocoracini <lorenzo291909@gmail.com>
2023-06-04 22:24:59 -0300

    Adição de produtos e combos em um pedido.

--- backend/src/controllers/PedidoController.ts
+++ backend/src/controllers/PedidoController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { pedidoRepository } from "../repositories/pedidoRepository";
-
+import { productRepository } from "../repositories/productRepository";
+import { comboRepository } from "../repositories/comboRepository";
 export class PedidoController {
   async create(req: Request, res: Response) {
     const { client_name } = req.body;
@@ -13,6 +14,63 @@ export class PedidoController {
       });
       await pedidoRepository.save(newPedido);
       return res.status(201).json(newPedido);
+    } catch (erro) {
+      console.log(erro);
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+  }
+  async addProduct(req: Request, res: Response) {
+    const { pedido_id, product_id } = req.params;
+
+    try {
+      const pedido = await pedidoRepository
+        .createQueryBuilder("pedido")
+        .leftJoinAndSelect("pedido.produtos", "produtos")
+        .where("pedido.id = :id", { id: pedido_id })
+        .getOne();
+      if (!pedido) {
+        return res.status(404).json({ message: "Pedido does not exist" });
+      }
+
+      const product = await productRepository.findOneBy({
+        id: Number(product_id),
+      });
+      if (!product) {
+        return res.status(404).json({ message: "Product does not exist" });
+      }
+
+      pedido.produtos.push(product);
+      await pedidoRepository.save(pedido);
+      return res.status(201).json(pedido.produtos);
+    } catch (erro) {
+      console.log(erro);
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+  }
+
+  async addCombo(req: Request, res: Response) {
+    const { pedido_id, combo_id } = req.params;
+
+    try {
+      const pedido = await pedidoRepository
+        .createQueryBuilder("pedido")
+        .leftJoinAndSelect("pedido.combos", "combos")
+        .where("pedido.id = :id", { id: pedido_id })
+        .getOne();
+      if (!pedido) {
+        return res.status(404).json({ message: "Pedido does not exist" });
+      }
+
+      const combo = await comboRepository.findOneBy({
+        id: Number(combo_id),
+      });
+      if (!combo) {
+        return res.status(404).json({ message: "Combo does not exist" });
+      }
+
+      pedido.combos.push(combo);
+      await pedidoRepository.save(pedido);
+      return res.status(201).json(pedido.combos);
     } catch (erro) {
       console.log(erro);
       return res.status(500).json({ message: "Internal Server Error" });
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -7,12 +7,23 @@ import { authMiddleware } from "./middlewares/authMiddleware";
 const routes = Router();
 
 routes.post("/produto", new ProdutoController().create);
+
 routes.post("/combo", new ComboController().create);
 routes.post(
   "/combo/:combo_id/add_product/:product_id",
   new ComboController().addProduct
 );
+
 routes.post("/pedido", new PedidoController().create);
+routes.post(
+  "/pedido/:pedido_id/add_product/:product_id",
+  new PedidoController().addProduct
+);
+routes.post(
+  "/pedido/:pedido_id/add_combo/:combo_id",
+  new PedidoController().addCombo
+);
+
 routes.post("/user", new UserController().create);
 routes.post("/login", new UserController().login);
 routes.get("/profile", authMiddleware, new UserController().getProfile);
