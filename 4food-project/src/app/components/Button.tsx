6d43a95d8cccb4f5c85e214f5067471b2c500fa9
John Doe <pedro.rocha2609@gmail.com>
2023-06-06 09:34:14 -0300

    footer e home, começando com framer motion

--- 4food-project/src/app/components/Button.tsx
+++ 4food-project/src/app/components/Button.tsx
@@ -2,12 +2,16 @@ import { type } from "os";
 import React from "react";
 
 type Props = {
-    children: string | JSX.Element
-}
+  children: string | JSX.Element;
+};
 
-export function Button({ children }:Props) {
-    return (
-        <button className='bg-black text-white px-14 py-4 hover:opacity-60 hover:-translate-y-1 
-        duration-300 text-xl'>{children}</button>
-    );
-  }
+export function Button({ children }: Props) {
+  return (
+    <button
+      className="bg-black text-white px-14 py-4 hover:opacity-80 hover:-translate-y-1 
+        duration-300 text-xl"
+    >
+      {children}
+    </button>
+  );
+}
---
+++ 4food-project/src/app/components/PhotosSection.tsx
@@ -0,0 +1,7 @@
+export function PhotoSection() {
+  return (
+    <section className="py-96">
+      <h1>Photos</h1>
+    </section>
+  );
+}
