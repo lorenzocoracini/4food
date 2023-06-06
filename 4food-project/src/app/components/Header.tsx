d74e3764a23d6597d0667cca552719036184c99d
Guilherme <guicardosodeoliveira@gmail.com>
2023-06-05 21:00:38 -0300

    Criação do componene botão e arrumar css tela login

--- 4food-project/src/app/components/Header.tsx
+++ 4food-project/src/app/components/Header.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import logo from '@images/logo-4food.png'
 import { AiOutlineShoppingCart } from 'react-icons/ai'
+import { Button } from './Button';
 
 export function Header() {
 
@@ -39,8 +40,9 @@ export function Header() {
 
       <div className='flex justify-center items-center gap-11'>
         <button  className='text-4xl hover:opacity-60 hover:scale-95 duration-300'><AiOutlineShoppingCart /></button>
-        <Link href="/login"><button className='bg-black text-white px-14 py-4 hover:opacity-60 hover:-translate-y-1 
-        duration-300 text-xl'>Entrar</button></Link>
+        <Link href="/login">
+          <Button>Entrar</Button>
+        </Link>
       </div>
     </div>
   );
