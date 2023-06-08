259d02acd3475971d3200bb841f9f164ba35acbf
Guilherme <guicardosodeoliveira@gmail.com>
2023-06-08 17:45:33 -0300

    Responsividade tela login

--- 4food-project/src/app/cardapio/page.tsx
+++ 4food-project/src/app/cardapio/page.tsx
@@ -3,7 +3,7 @@ import exemplo from "@images/sushiexemplo.jpg"
 
 export default function Cardapio() {
   return (
-    <div className="w-full p-8 md:grid md:grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-2 items-center 
+    <div className="w-full py-8 px-32 md:grid md:grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-4 items-center 
     justify-center lg:flex lg:flex-wrap">
       <ItemMenu price={19.99} name="Sushi" image={exemplo} description="Sushizinho muito saboroso"/>
       <ItemMenu price={19.99} name="Sushi" image={exemplo} description="Sushizinho muito saboroso"/>
