21f7021f4560dd1a2a2a4e495d140d3db4e0fdae
John Doe <pedro.rocha2609@gmail.com>
2023-06-10 10:04:40 -0300

    home quase finalizada e responsividade ok

--- 4food-project/src/app/cardapio/page.tsx
+++ 4food-project/src/app/cardapio/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { ItemMenu } from "../components/ItemMenu";
 import exemplo from "@images/sushiexemplo.jpg";
 import CSS from "csstype";
@@ -6,16 +6,15 @@ import { color } from "framer-motion";
 import { useState } from "react";
 
 export default function Cardapio() {
-
-  const [activeItem, setActiveItem] = useState('principal')
+  const [activeItem, setActiveItem] = useState("principal");
 
   const handleClick = (item: string) => {
-    setActiveItem(item)
-  }
+    setActiveItem(item);
+  };
 
-  const styleButtonActive = 'text-black bg-white border border-black'
-  
-  const styleButtonNotActive = 'text-white bg-black'
+  const styleButtonActive = "text-black bg-white border border-black";
+
+  const styleButtonNotActive = "text-white bg-black";
 
   return (
     <section>
@@ -24,32 +23,107 @@ export default function Cardapio() {
         <h1 className="text-4xl font-bold">NOSSO MENU</h1>
       </div>
       <div className="flex justify-center gap-2 my-3">
-        <button className={`px-14 py-4 hover:opacity-80 hover:-translate-y-1 
-        duration-300 text-xl ${activeItem === 'principal' ? styleButtonActive : styleButtonNotActive}`} 
-        onClick={() => handleClick("principal")}>PRINCIPAIS</button>
-        <button className={`px-14 py-4 hover:opacity-80 hover:-translate-y-1 
-        duration-300 text-xl ${activeItem === 'sushi' ? styleButtonActive : styleButtonNotActive}`} 
-        onClick={() => handleClick("sushi")}>SUSHI</button>
-        <button className={`px-14 py-4 hover:opacity-80 hover:-translate-y-1 
-        duration-300 text-xl ${activeItem === 'bebidas' ? styleButtonActive : styleButtonNotActive}`} 
-        onClick={() => handleClick("bebidas")}>BEBIDAS</button>
-        <button className={`px-14 py-4 hover:opacity-80 hover:-translate-y-1 
-        duration-300 text-xl ${activeItem === 'combos' ? styleButtonActive : styleButtonNotActive}`} 
-        onClick={() => handleClick("combos")}>COMBOS</button>
-        
+        <button
+          className={`px-14 py-4 hover:opacity-80 hover:-translate-y-1 
+        duration-300 text-xl ${
+          activeItem === "principal" ? styleButtonActive : styleButtonNotActive
+        }`}
+          onClick={() => handleClick("principal")}
+        >
+          PRINCIPAIS
+        </button>
+        <button
+          className={`px-14 py-4 hover:opacity-80 hover:-translate-y-1 
+        duration-300 text-xl ${
+          activeItem === "sushi" ? styleButtonActive : styleButtonNotActive
+        }`}
+          onClick={() => handleClick("sushi")}
+        >
+          SUSHI
+        </button>
+        <button
+          className={`px-14 py-4 hover:opacity-80 hover:-translate-y-1 
+        duration-300 text-xl ${
+          activeItem === "bebidas" ? styleButtonActive : styleButtonNotActive
+        }`}
+          onClick={() => handleClick("bebidas")}
+        >
+          BEBIDAS
+        </button>
+        <button
+          className={`px-14 py-4 hover:opacity-80 hover:-translate-y-1 
+        duration-300 text-xl ${
+          activeItem === "combos" ? styleButtonActive : styleButtonNotActive
+        }`}
+          onClick={() => handleClick("combos")}
+        >
+          COMBOS
+        </button>
       </div>
-      <div className="w-full py-8 px-32 md:grid md:grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-4 items-center 
-      justify-center lg:flex lg:flex-wrap">
-        <ItemMenu price={19.99} name="Sushi" image={exemplo} description="Sushizinho muito saboroso"/>
-        <ItemMenu price={19.99} name="Sushi" image={exemplo} description="Sushizinho muito saboroso"/>
-        <ItemMenu price={19.99} name="Sushi" image={exemplo} description="Sushizinho muito saboroso"/>
-        <ItemMenu price={19.99} name="Sushi" image={exemplo} description="Sushizinho muito saboroso"/>
-        <ItemMenu price={19.99} name="Sushi" image={exemplo} description="Sushizinho muito saboroso"/>
-        <ItemMenu price={19.99} name="Sushi" image={exemplo} description="Sushizinho muito saboroso"/>
-        <ItemMenu price={19.99} name="Sushi" image={exemplo} description="Sushizinho muito saboroso"/>
-        <ItemMenu price={19.99} name="Sushi" image={exemplo} description="Sushizinho muito saboroso"/>
-        <ItemMenu price={19.99} name="Sushi" image={exemplo} description="Sushizinho muito saboroso"/>
-        <ItemMenu price={19.99} name="Sushi" image={exemplo} description="Sushizinho muito saboroso"/>
+      <div
+        className="w-full py-8 px-32 md:grid md:grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-4 items-center 
+      justify-center lg:flex lg:flex-wrap"
+      >
+        <ItemMenu
+          price={19.99}
+          name="Sushi"
+          image={exemplo}
+          description="Sushizinho muito saboroso"
+        />
+        <ItemMenu
+          price={19.99}
+          name="Sushi"
+          image={exemplo}
+          description="Sushizinho muito saboroso"
+        />
+        <ItemMenu
+          price={19.99}
+          name="Sushi"
+          image={exemplo}
+          description="Sushizinho muito saboroso"
+        />
+        <ItemMenu
+          price={19.99}
+          name="Sushi"
+          image={exemplo}
+          description="Sushizinho muito saboroso"
+        />
+        <ItemMenu
+          price={19.99}
+          name="Sushi"
+          image={exemplo}
+          description="Sushizinho muito saboroso"
+        />
+        <ItemMenu
+          price={19.99}
+          name="Sushi"
+          image={exemplo}
+          description="Sushizinho muito saboroso"
+        />
+        <ItemMenu
+          price={19.99}
+          name="Sushi"
+          image={exemplo}
+          description="Sushizinho muito saboroso"
+        />
+        <ItemMenu
+          price={19.99}
+          name="Sushi"
+          image={exemplo}
+          description="Sushizinho muito saboroso"
+        />
+        <ItemMenu
+          price={19.99}
+          name="Sushi"
+          image={exemplo}
+          description="Sushizinho muito saboroso"
+        />
+        <ItemMenu
+          price={19.99}
+          name="Sushi"
+          image={exemplo}
+          description="Sushizinho muito saboroso"
+        />
       </div>
     </section>
   );
