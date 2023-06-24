ac52f1e5c0fe446faed2facc8090b035dc2f59a3
Guilherme <guicardosodeoliveira@gmail.com>
2023-06-24 09:00:32 -0300

    feat: responsividade da pgn sobre

--- 4food-project/src/app/components/OurHistory.tsx
+++ 4food-project/src/app/components/OurHistory.tsx
@@ -4,26 +4,26 @@ import Image from 'next/image'
 
 export function OurHistory () {
     return (
-        <div className='flex mx-20 mt-32 pb-20 h-screen border-b border-gray-300'>
-            <div className='flex flex-col justify-center items-center gap-3 w-2/5 relative'>
-                <Image src={restaurante} alt='Imagem ilustrativa Restaurante' width={320} className='absolute top-0'/>
-                <Image src={ilustração} alt='Imagem ilustrativa pessoas comendo' width={320} className='absolute bottom-0 right-5'/>
+        <div className='flex flex-col-reverse lg:flex-row lg:mx-24 mx-8 lg:mt-32 lg:pb-32 border-b border-gray-300'>
+            <div className='flex lg:flex-col flex-row justify-center lg:items-center gap-3 lg:w-2/5 lg:relative mt-5 lg:mt-0'>
+                <Image src={restaurante} alt='Imagem ilustrativa Restaurante' className='lg:absolute lg:top-0 lg:w-80 w-36'/>
+                <Image src={ilustração} alt='Imagem ilustrativa pessoas comendo' className='lg:absolute lg:top-[335px] lg:right-5 lg:w-80 w-36'/>
             </div>
-            <div className='w-3/5 pl-8'>
-                <h2 className='text-4xl uppercase font-bold pb-8'>Nossa História</h2>
-                <div className='max-w-2xl text'>
-                    <p className='leading-7 text-xl text-gray-400 indent-6'>
+            <div className='lg:w-3/5 lg:pl-8'>
+                <h2 className='lg:text-4xl text-2xl uppercase font-bold pb-8'>Nossa História</h2>
+                <div className='lg:max-w-2xl'>
+                    <p className='leading-7 text-base text-gray-400 indent-6'>
                     Em 2015, quatro jovens empreendedores com uma paixão compartilhada pela culinária japonesa decidiram 
                     transformar seu sonho em realidade. Assim nasceu o 4Food, um restaurante que trazia consigo a promessa 
                     de oferecer uma experiência única e autêntica aos amantes da gastronomia japonesa.
                     </p>
-                    <p className='leading-7 text-xl text-gray-400 indent-6'>
+                    <p className='leading-7 text-base text-gray-400 indent-6'>
                     Ao longo dos anos, o 4Food cresceu e se tornou um destino popular para os amantes de sushi e culinária 
                     japonesa. A dedicação dos quatro fundadores, juntamente com a equipe talentosa que se juntou a eles, 
                     resultou em pratos excepcionais que encantam os clientes e contam a história da paixão e do compromisso 
                     por trás de cada mordida.
                     </p>
-                    <p className='leading-7 text-xl text-gray-400 indent-6'>
+                    <p className='leading-7 text-base text-gray-400 indent-6'>
                     Hoje, o 4Food continua a ser um lugar onde a tradição se encontra com a inovação, onde os sabores 
                     autênticos da culinária japonesa são celebrados em um ambiente acolhedor. A história desse restaurante 
                     único continua a ser escrita, sempre com a mesma paixão e dedicação que o tornaram um sucesso desde o 
