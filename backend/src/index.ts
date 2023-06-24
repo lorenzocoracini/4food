9642f5ed433b5731a25eb19ef8e854be3109dd04
lorenzocoracini <lorenzo291909@gmail.com>
2023-06-24 14:31:34 -0300

    cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,11 +3,13 @@ import express from "express";
 import { AppDataSource } from "./data-source";
 import routes from "./routes";
 import { errorMiddleware } from "./middlewares/error";
+import cors from "cors";
 
 AppDataSource.initialize().then(() => {
   const app = express();
 
   app.use(express.json());
+  app.use(cors({ exposedHeaders: ["Authorization"] }));
   app.use(routes);
   app.use(errorMiddleware);
 
