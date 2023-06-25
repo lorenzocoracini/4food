129d8c7c108788a8ec761ad6980838953f4a44f2
John Doe <pedro.rocha2609@gmail.com>
2023-06-25 16:11:45 -0300

    Requisições começando a serem implementadas

--- 4food-project/src/app/cadastro/page.tsx
+++ 4food-project/src/app/cadastro/page.tsx
@@ -1,40 +1,146 @@
-'use client'
+"use client";
+
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import { LuAlertOctagon } from "react-icons/lu";
+
+import * as Yup from "yup";
+
+const schemaPedido = Yup.object().shape({
+  name: Yup.string().required("Informe sua Nome."),
+  email: Yup.string().required("Informe seu Email"),
+  phone: Yup.string().required("Informe seu Telefone"),
+  password: Yup.string().required("Infome uma Senha"),
+});
+
+
 
 export default function Cadastro() {
-    return (
-      <section className="h-screen w-full flex justify-center items-center bg-[url('../../public/images/background-tela-login.jpg')]
-      bg-cover bg-center relative mt-5 lg:mt-8">
-        <div className="flex flex-col justify-center items-center bg-white bg-opacity-80
-         text-black absolute rounded w-3/4 lg:p-4 lg:w-2/3 lg:h-auto">
-            <h1 className="text-2xl lg:text-3xl my-8 border-b border-black border-solid">Cadastre-se</h1>
-            <form action="" className="flex justify-center flex-col gap-5">
-                <div className='flex flex-col items-center lg:items-start mb-2 px-3 lg:px-0'>
-                    <label htmlFor="input_nome" className='block text-base mb-2 lg:text-xl'>Nome</label>
-                    <input id="input_nome" type="text" placeholder="Digite seu Nome" className='py-3 px-4 w-4/5 lg:w-72
-                    bg-white text-black border border-black border-solid'/>
-                </div>
-                <div className='flex flex-col items-center lg:items-start mb-2 px-3 lg:px-0'>
-                    <label htmlFor="input_email" className='block text-base mb-2 lg:text-xl'>Email</label>
-                    <input id="input_email" type="email" placeholder="Digite seu email" className='py-3 px-4 w-4/5 lg:w-72
-                    bg-white text-black border border-black border-solid'/>
-                </div>
-                <div className='flex flex-col items-center lg:items-start mb-2 px-3 lg:px-0'>
-                    <label htmlFor="input_telefone" className='block text-base mb-2 lg:text-xl'>Telefone</label>
-                    <input id="input_telefone" type="tel" placeholder="(xx) xxxx-xxxx" className='py-3 px-4 w-4/5 lg:w-72
-                    bg-white text-black border border-black border-solid'/>
-                </div>
-                <div className='flex flex-col items-center lg:items-start mb-2 px-3 lg:px-0'>
-                    <label htmlFor="input_senha" className='block text-base mb-2 lg:text-xl'>Senha</label>
-                    <input id="input_senha" type="password" placeholder="Digite uma senha" className='py-3 px-4 w-4/5 lg:w-72
-                    bg-white text-black border border-black border-solid'/>
-                </div>
-                <div className="flex flex-col px-3 lg:px-0 items-center lg:items-start mb-5">
-                    <button className=' w-4/5 lg:w-72 py-3 bg-black text-white hover:opacity-80'>Criar Conta</button>
-                    <p className="text-xs lg:text-sm">voltar para tela de <a href="/login" className="border-b border-solid">Login</a></p>
-                </div>
-            </form>
-        </div>
-      </section>
-    );
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    mode: "onSubmit",
+    resolver: yupResolver(schemaPedido),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+      password: "",
+    },
+  });
+
+  function handleSubmitData(data: any) {
+    getData(data);
   }
-  
+
+  return (
+    <section
+      className="h-screen w-full flex justify-center items-center bg-[url('../../public/images/background-tela-login.jpg')]
+      bg-cover bg-center relative mt-5 lg:mt-8"
+    >
+      <div
+        className="flex flex-col justify-center items-center bg-white bg-opacity-95
+         text-black absolute rounded w-3/4 lg:p-4 lg:w-33 lg:h-auto"
+      >
+        <h1 className="text-2xl lg:text-3xl my-8 border-b border-black border-solid">
+          Cadastre-se
+        </h1>
+        <form
+          onSubmit={handleSubmit(handleSubmitData)}
+          className="flex justify-center flex-col gap-5"
+        >
+          <div className="flex flex-col items-center lg:items-start mb-2 px-3 lg:px-0">
+            <label htmlFor="nome" className="block text-base mb-2 lg:text-xl">
+              Nome
+            </label>
+            <input
+              type="text"
+              placeholder="Digite seu Nome"
+              {...register("name")}
+              className="py-3 px-4 w-4/5 lg:w-72
+                    bg-white text-black border border-black border-solid mb-2"
+            />
+            {errors.name ? (
+              <p className="bg-red-600 text-white text-sm lg:text-md p-2 w-4/5 lg:w-72 rounded flex gap-2 items-center mb-2">
+                <LuAlertOctagon size={20} />
+                {errors.name?.message}
+              </p>
+            ) : null}
+          </div>
+          <div className="flex flex-col items-center lg:items-start mb-2 px-3 lg:px-0">
+            <label htmlFor="email" className="block text-base mb-2 lg:text-xl">
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              {...register("email")}
+              placeholder="Digite seu email"
+              className="py-3 px-4 w-4/5 lg:w-72 mb-2
+                    bg-white text-black border border-black border-solid"
+            />
+            {errors.email ? (
+              <p className="bg-red-600 text-white text-sm lg:text-md p-2 rounded flex gap-2 items-center mb-2 w-4/5 lg:w-72">
+                <LuAlertOctagon size={20} />
+                {errors.email?.message}
+              </p>
+            ) : null}
+          </div>
+          <div className="flex flex-col items-center lg:items-start mb-2 px-3 lg:px-0">
+            <label
+              htmlFor="telefone"
+              className="block text-base mb-2 lg:text-xl"
+            >
+              Telefone
+            </label>
+            <input
+              id="telefone"
+              {...register("phone")}
+              type="tel"
+              placeholder="(xx) xxxx-xxxx"
+              className="py-3 px-4 w-4/5 lg:w-72 mb-2
+                    bg-white text-black border border-black border-solid"
+            />
+            {errors.phone ? (
+              <p className="bg-red-600 text-white text-sm lg:text-md p-2 rounded flex gap-2 items-center mb-2 w-4/5 lg:w-72">
+                <LuAlertOctagon size={20} />
+                {errors.phone?.message}
+              </p>
+            ) : null}
+          </div>
+          <div className="flex flex-col items-center lg:items-start mb-2 px-3 lg:px-0">
+            <label htmlFor="senha" className="block text-base mb-2 lg:text-xl">
+              Senha
+            </label>
+            <input
+              id="senha"
+              {...register("password")}
+              type="password"
+              placeholder="Digite uma senha"
+              className="py-3 px-4 w-4/5 lg:w-72 mb-2
+                    bg-white text-black border border-black border-solid"
+            />
+            {errors.password ? (
+              <p className="bg-red-600 text-white text-sm lg:text-md p-2 rounded flex gap-2 items-center mb-2 w-4/5 lg:w-72">
+                <LuAlertOctagon size={20} />
+                {errors.password?.message}
+              </p>
+            ) : null}
+          </div>
+          <div className="flex flex-col px-3 lg:px-0 items-center lg:items-start mb-5">
+            <button className=" w-4/5 lg:w-72 py-3 bg-black text-white hover:opacity-80">
+              {isSubmitting ? "Enviando..." : "Criar Conta"}
+            </button>
+            <p className="text-xs lg:text-sm">
+              <a href="/login" className="border-b border-solid">
+                Login
+              </a>
+            </p>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+}
--- 4food-project/src/app/login/page.tsx
+++ 4food-project/src/app/login/page.tsx
@@ -1,56 +1,98 @@
-'use client'
+"use client";
 import { useForm } from "react-hook-form";
-import * as Yup from 'yup';
-import { yupResolver } from '@hookform/resolvers/yup'
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { LuAlertOctagon } from "react-icons/lu";
 
 export default function Login() {
+  const schema = Yup.object().shape({
+    email: Yup.string()
+      .required("Campo obrigatório!")
+      .email("O dado fornecido deve ser um email válido."),
+    password: Yup.string().required("Campo obrigatório!"),
+  });
 
-    const schema = Yup.object().shape({
-      email: Yup.string().required('Campo obrigatório!').email('O dado fornecido deve ser um email válido.'),
-      password: Yup.string().required('Campo obrigatório!')
-    })
+  const { register, handleSubmit, formState } = useForm({
+    mode: "onSubmit",
+    resolver: yupResolver(schema),
+  });
 
-    const { register, handleSubmit, formState } = useForm({
-      mode: 'onSubmit',
-      resolver: yupResolver(schema)
-    })
+  const { errors, isSubmitting } = formState;
 
-    const { errors, isSubmitting } = formState;
+  const handleSubmitLogin = (data: any) => {
+    console.log("submit", data);
+  };
 
-    const handleSubmitLogin = (data: any) => {
-      console.log('submit', data);
-    }
-
-    return (
-      <section className="h-screen w-full flex justify-center items-center bg-[url('../../public/images/background-tela-login.jpg')]
-      bg-cover bg-center relative">
-        <div className="flex justify-center items-center flex-col bg-white bg-opacity-80
-         text-black absolute rounded max-w-md w-3/4 h-2/4 lg:p-4 lg:w-5/12 lg:h-3/4">
-          <h1 className="text-2xl lg:text-3xl p-2 lg:absolute lg:top-16 border-b border-black border-solid">Faça o Login</h1>
-          <form onSubmit={handleSubmit(handleSubmitLogin)} className="mt-4">
-            <div className='mb-2 px-3 lg:px-0'>
-              <label htmlFor="input_email" className='block text-base mb-2 lg:text-xl'>Email</label>
-              <input id="input_email" type="text" placeholder="Digite seu email" className='py-3 px-4 w-4/5 lg:w-72
-              bg-black text-white' {...register('email')}/>
-              {errors.email && (
-                <p className="text-red-500">{errors.email.message}</p>
-              )}
-            </div>
-            <div className='mb-2 px-3 lg:px-0'>
-              <label htmlFor="input_senha" className='block text-base mb-2 lg:text-xl'>Senha</label>
-              <input id="input_senha" type="password" placeholder="Digite sua senha" className='py-3 px-4 w-4/5 lg:w-72
-              bg-black text-white active:border-none' {...register('password')}/>
-              {errors.password && (
-                <p className="text-red-500">{errors.password.message}</p>
-              )}
-            </div>
-            <div className="px-3 lg:px-0">
-              <button className=' w-4/5 h-12 lg:w-72 lg:h-12 bg-black text-white mt-4 hover:opacity-80'>Entrar</button>
-            </div>
-            <p className="text-xs lg:text-sm mt-2">Não possui conta? <a href="/cadastro" className="border-b border-black border-solid">Cadastre-se</a></p>
-          </form>
-        </div>
-      </section>
-    );
-  }
-  
+  return (
+    <section
+      className="h-screen w-full flex justify-center items-center bg-[url('../../public/images/background-tela-login.jpg')]
+      bg-cover bg-center relative"
+    >
+      <div
+        className="flex justify-center items-center flex-col bg-white bg-opacity-95
+         text-black absolute rounded max-w-md w-3/4 h-2/4 lg:p-4 lg:w-5/12 lg:h-3/4"
+      >
+        <h1 className="text-2xl lg:text-3xl p-2 lg:absolute lg:top-16 border-b border-black border-solid">
+          Faça o Login
+        </h1>
+        <form onSubmit={handleSubmit(handleSubmitLogin)} className="mt-4">
+          <div className="mb-2 px-3 lg:px-0">
+            <label
+              htmlFor="input_email"
+              className="block text-base mb-2 lg:text-xl"
+            >
+              Email
+            </label>
+            <input
+              id="input_email"
+              type="text"
+              placeholder="Digite seu email"
+              className="py-3 px-4 w-4/5 lg:w-72 mb-2
+              bg-black text-white"
+              {...register("email")}
+            />
+            {errors.email && (
+              <p className="bg-red-600 text-white text-sm lg:text-md p-2 rounded flex gap-2 items-center mb-2 w-4/5 lg:w-72">
+                <LuAlertOctagon size={20} />
+                {errors.email?.message}
+              </p>
+            )}
+          </div>
+          <div className="mb-2 px-3 lg:px-0">
+            <label
+              htmlFor="input_senha"
+              className="block text-base mb-2 lg:text-xl"
+            >
+              Senha
+            </label>
+            <input
+              id="input_senha"
+              type="password"
+              placeholder="Digite sua senha"
+              className="py-3 px-4 w-4/5 lg:w-72 mb-2
+              bg-black text-white active:border-none"
+              {...register("password")}
+            />
+            {errors.password && (
+              <p className="bg-red-600 text-white text-sm lg:text-md p-2 rounded flex gap-2 items-center mb-2 w-4/5 lg:w-72">
+                <LuAlertOctagon size={20} />
+                {errors.password?.message}
+              </p>
+            )}
+          </div>
+          <div className="px-3 lg:px-0">
+            <button className=" w-4/5 h-12 lg:w-72 lg:h-12 bg-black text-white mt-4 hover:opacity-80">
+              Entrar
+            </button>
+          </div>
+          <p className="text-xs lg:text-sm mt-2">
+            Não possui conta?{" "}
+            <a href="/cadastro" className="border-b border-black border-solid">
+              Cadastre-se
+            </a>
+          </p>
+        </form>
+      </div>
+    </section>
+  );
+}
